8bd2222071dac7352189038d651b876fdeb6c0c8
Majinice <nurumajidadi@gmail.com>
2016-10-16 00:04:16 +0100

    I've added ten test caeses

--- tests.js
+++ tests.js
@@ -27,24 +27,29 @@ describe("Test that constants are computed properly", function() {
     assert(
       lib.sum_of_prime('s') == false
   }); 
-   it("should give 17, for an input of 10", function() {
+   it("should give 1060, for an input of 100", function() {
     assert(
       lib.sum_of_prime(100) == 1060
     );
   });
-  it("should give 41, for an input of 15", function() {
+  it("should give 2276, for an input of 150", function() {
     assert(
       lib.sum_of_prime(150) == 2276
     );
   });
-   it("should give 17, for an input of 10", function() {
+   it("should give 1593, for an input of 120", function() {
     assert(
       lib.sum_of_prime(120) == 1593
     );
   });
-  it("should give 41, for an input of 15", function() {
+  it("should give 21536, for an input of 500", function() {
     assert(
       lib.sum_of_prime(500) == 21536
+    );
+  });
+  it("should give 17, for an input of 7", function() {
+    assert(
+      lib.sum_of_prime(7) == 17
     );
   });
 });
