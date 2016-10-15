ddb2daf0e0107e73928874073158a9512ef748d3
Majinice <nurumajidadi@gmail.com>
2016-10-15 23:31:52 +0100

    I've added five test cases

--- tests.js
+++ tests.js
@@ -1,24 +1,30 @@
-'use strict'
-
 var chai = require('chai');
 var assert = chai.assert;
 
-var lib = require('./lib/library.js');
+var lib = require('./lib/sum_of_file.js');
 
 describe("Test that constants are computed properly", function() {
-  it("should give 10, 4 for constants 2, 5 as a and n respectively", function() {
+  it("should give 10, for an input of 5", function() {
     assert(
-      lib.compareCoefficients(lib.quadraticDerivative(2, 5), { a: 10, n: 4 })
+      lib.sum_of_prime(5) == 10
     );
   });
-  it("should give 2, 1 for constants 1, 2 as a and n respectively", function() {
+  it("should give 17, for an input of 10", function() {
     assert(
-      lib.compareCoefficients(lib.quadraticDerivative(1, 2), { a: 2, n: 1 })
+      lib.sum_of_prime(10) == 17
     );
   });
-  it("should give 8, 1 for constants 4, 2 as a and n respectively", function() {
+  it("should give 41, for an input of 15", function() {
     assert(
-      lib.compareCoefficients(lib.quadraticDerivative(4, 2), { a: 8, n: 1 })
+      lib.sum_of_prime(15) == 41
     );
   });
+  it("should give false, for an input of negative number", function() {
+    assert(
+      lib.sum_of_prime(-25) == false
+  });
+  it("should give false, for an input of a string", function() {
+    assert(
+      lib.sum_of_prime('s') == false
+  });  
 });
